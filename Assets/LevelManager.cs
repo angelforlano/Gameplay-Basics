6bd417aa47ac91541eaeb1cc34c9b10be02458cf
Angel-Lap <angel12345leo@gmail.com>
2020-10-21 11:41:55 +0200

    + LevelManager

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public static LevelManager Instance;
+    
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+
+    public void LoadLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+    }
+}
---
+++ Assets/LoadSceneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadSceneTrigger : MonoBehaviour
+{
+    public string levelToLoad;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            LevelManager.Instance.LoadLevel(levelToLoad);
+        }
+    }
+}
